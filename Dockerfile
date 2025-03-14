8bce948994f7daedf03db086f448b90154761120
Jorge Guerra Canal <jorge.guerra.canal@gmail.com>
2025-03-14 15:37:51 +0100

    gunicorn config

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 EXPOSE 8080
 
-CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 wsgi:app
+CMD exec gunicorn --bind 0.0.0.0:$PORT --workers 1 --threads 8 --timeout 120 --access-logfile - --error-logfile - wsgi:app
--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,4 @@
-import os
-from app import app
+from app import app  # Asegúrate de que "app" sea la instancia de Flask
 
-# Convertir la variable PORT a entero, en caso de que esté como cadena
-port = int(os.getenv('PORT', 8080))
 if __name__ == "__main__":
-    # Usa el valor de la variable 'port' en lugar de un valor fijo
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run()  # Esto solo se ejecuta localmente, Gunicorn ignora este bloque
