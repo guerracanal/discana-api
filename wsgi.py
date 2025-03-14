94b73615a2a9a5244fb1ef3b0244a298b74773e6
Jorge Guerra Canal <jorge.guerra.canal@gmail.com>
2025-03-14 14:57:42 +0100

    debug false

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,8 @@
 import os
 from app import app
 
+# Convertir la variable PORT a entero, en caso de que esté como cadena
 port = int(os.getenv('PORT', 8080))
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=port, debug=True)
+    # Usa el valor de la variable 'port' en lugar de un valor fijo
+    app.run(host="0.0.0.0", port=port, debug=False)
