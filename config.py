2085416d32a94c427a87636712d2fdd9ef179c9e
Jorge Guerra Canal <jorge.guerra.canal@gmail.com>
2025-03-14 15:48:14 +0100

    fix dotenv

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
+# config.py
 import os
-from dotenv import load_dotenv
 
-# Cargar variables de entorno desde el archivo .env
-load_dotenv()
+# Cargar .env solo si existe (para desarrollo local)
+if os.path.exists(".env"):
+    from dotenv import load_dotenv
+    load_dotenv()
 
 class Config:
-    MONGO_URI = os.getenv("MONGO_URI")  # Esta variable se tomará del archivo .env o del entorno del sistema
-    DEBUG = True
+    MONGO_URI = os.getenv("MONGO_URI")
+    # ... otras variables
