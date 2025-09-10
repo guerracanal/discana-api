5f73844b3b92c1e5c282cb2bf425478db799ce8d
Jorge Guerra Canal <jorge.guerra.canal@gmail.com>
2025-09-10 16:53:43 +0200

    timeout

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,6 +4,6 @@ import os
 bind = f"0.0.0.0:{os.environ.get('PORT', '8080')}"
 workers = 1         # Inicio rápido, luego Cloud Run escala si es necesario
 threads = 1
-timeout = 0         # No matar workers mientras arranca la app
-graceful_timeout = 30
+timeout = 300         # Aumenta el tiempo de espera para arranque lento
+graceful_timeout = 300
 preload_app = False  # Muy importante: no pre-cargar librerías pesadas
